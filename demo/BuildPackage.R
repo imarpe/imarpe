79f0e676889f0891b50ad6dfca17b312a7ee1f13
VilmaRomero <vilsurr@gmail.com>
2014-09-10 12:09:07 -0400

    Build Package script

--- demo/BuildPackage.R
+++ demo/BuildPackage.R
@@ -3,5 +3,5 @@ library(roxygen2)
 roxygenize("/home/vromero/VILMA/PROJECT_IMARPE_TOOLS/imarpeTools",roclets= c("collate", "rd"))
 
 # 2.Building and checking the package through the terminal
-system('R CMD build /home/vromero/VILMA/PROJECT_IMARPE_TOOLS/imarpeTools')
-system('R CMD check /home/vromero/VILMA/PROJECT_IMARPE_TOOLS/imarpeTools/imarpe_0.1.tar.gz')
+system(paste0("R CMD build ",getwd(),sep=""))
+system(paste0("R CMD check ",getwd(),"/imarpe_0.1.tar.gz",sep=""))
