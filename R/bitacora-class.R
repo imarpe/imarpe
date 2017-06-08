f7c34bf4cc72fd1394b36f5f9d8b9caf755841bc
criscely <criscelylujan@gmail.com>
2017-06-08 14:49:33 -0500

    Mistakes in print.bitacora about the fleets.

--- R/bitacora-class.R
+++ R/bitacora-class.R
@@ -1,5 +1,4 @@
 
-
 print.bitacora = function(x, language="spanish") {
 
   if(language == "english"){
@@ -8,14 +7,14 @@ print.bitacora = function(x, language="spanish") {
     cat("Number of ports of data: ", x$info$ports, "\n", sep="")
     cat("Number of months of data: ", x$info$months, "\n", sep="")
     cat("Number of years of data: ", x$info$years, "\n", sep="")
-    cat("Types of fleet: ", x$info$fleets, "\n", sep="")
+    cat("Types of fleet:", x$info$fleets, "\n", sep="  ")
   } else {
     cat("Datos de : ", sQuote(x$info$file), "\n", sep="")
     cat("Numero de viajes: ", x$info$trips, "\n", sep="")
     cat("Numero de puertos: ", x$info$ports, "\n", sep="")
     cat("Numero de meses de la data: ", x$info$months, "\n", sep="")
     cat("Numero de ahnos de la data: ", x$info$years, "\n", sep="")
-    cat("Tipos de flota: ",x$info$fleets, "\n", sep="")
+    cat("Tipos de flota:", x$info$fleets , "\n", sep="  ")
   }
 
   return(invisible())
