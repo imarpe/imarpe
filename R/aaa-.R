e801755eecde44b372ecf5eaf30e2cd30d84ce56
roliveros-ramos <ricardo.oliveros@gmail.com>
2017-08-25 17:33:41 +0200

    Setting imports as firt load.

---
+++ R/aaa-.R
@@ -0,0 +1,12 @@
+#' @import reshape2
+#' @import plyr
+#' @import rmarkdown
+#' @import maps
+#' @import fields
+#' @import sp
+#' @import rJava
+#' @importFrom plotrix pie3D
+#' @importFrom Hmisc monthDays yearDays
+#' @importFrom RCurl url.exists
+#' @importFrom XLConnect loadWorkbook readWorksheet
+#' @importFrom lubridate day
