68b386f2ddb0bdb4bc6b210eff975baff9e4c843
roliveros-ramos <ricardo.oliveros@gmail.com>
2017-08-25 16:41:40 +0200

    Templates for class 'sisesat'

---
+++ R/sisesat-class.R
@@ -0,0 +1,3 @@
+# Main functions for the 'sisesat' class
+# includes: a function to read data and create the object of class
+# sisesat
---
+++ R/sisesat-internal.R
@@ -0,0 +1,1 @@
+# functions used for the sisesat class but not exported (available to the user)
